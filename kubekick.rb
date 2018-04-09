aaad11ddfdd484eed0f743a9970cdc5251932b9c
Ray Zane <ray@promptworks.com>
2018-04-09 16:18:58 -0400

    v1.0.0

--- kubekick.rb
+++ kubekick.rb
@@ -2,9 +2,9 @@ class Kubekick < Formula
   desc "One-off tasks and encrypted secrets CLI for Kubernetes"
   homepage "https://github.com/rzane/kubekick"
 
-  version "0.1.1"
-  url "https://github.com/rzane/kubekick/releases/download/v0.1.1/kubekick-0.1.1_darwin_x86_64.tar.gz"
-  sha256 "c3ba5bc846c503cd9ad51f404fa1b284acd226d004104090bd16c2a574834076"
+  version "1.0.0"
+  url "https://github.com/rzane/kubekick/releases/download/v1.0.0/kubekick-1.0.0_darwin_x86_64.tar.gz"
+  sha256 "ee03f9734b6da3a4284c95cb4f42fa6b3232e2679e1eb20d7bfa3655532269ab"
 
   def install
     bin.install "kubekick"
